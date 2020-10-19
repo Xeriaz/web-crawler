5788fa9348c450d819b671f77431bfd47a248ce9
Linas Linartas <lilinartas@gmail.com>
2020-10-19 14:46:30 +0300

    Crawler Refactoring second iteration.

--- src/Crawler/Crawler.php
+++ src/Crawler/Crawler.php
@@ -3,166 +3,35 @@
 namespace App\Crawler;
 
 use App\Entity\Link;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DomCrawler\Crawler as DomCrawler;
-use Symfony\Component\Workflow\Registry;
+use App\Manager\LinkManager;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use Symfony\Contracts\HttpClient\ResponseInterface;
-use Throwable;
 
 class Crawler
 {
-    /**
-     * @var EntityManagerInterface
-     */
-    private $em;
+    private LinkManager $linkManager;
 
-    /**
-     * @var HttpClientInterface
-     */
-    private $httpClient;
+    private HttpClientInterface $httpClient;
 
-    /**
-     * @var Registry
-     */
-    private $registry;
+    private ResponseParser $responseParser;
 
     public function __construct(
-        EntityManagerInterface $em,
+        LinkManager $linkManager,
         HttpClientInterface $httpClient,
-        Registry $registry
+        ResponseParser $responseParser
     ) {
-        $this->em = $em;
+        $this->linkManager = $linkManager;
         $this->httpClient = $httpClient;
-        $this->registry = $registry;
+        $this->responseParser = $responseParser;
     }
-
-    protected static $crawledUrls = [];
 
     public function crawl(string $url, ?Link $parentLink = null): void
     {
-        dump($url);
-        $link = $this->fetchOrCreateLink($url, $parentLink);
+        $link = $this->linkManager->fetchOrCreateLink($url, $parentLink);
 
-        $response = $this->httpClient->request(
-            'GET',
-            $link->getLink()
-        );
+        $response = $this->httpClient->request('GET', $link->getLink());
 
-        $this->updateLinkWithResponse($link, $response);
+        $this->linkManager->updateLinkWithResponse($link, $response);
 
-        $content = $this->resolveContent($response);
-
-        if ($content === null) {
-            $this->em->persist($link);
-            $this->em->flush();
-
-            return;
-        }
-
-        self::$crawledUrls[] = $url;
-
-        $linksInContent = (new DomCrawler($content, $url))
-            ->filter('a')
-            ->links();
-
-        foreach ($linksInContent as $linkInContent) {
-            sleep(1);
-
-            $linkUrl = $linkInContent->getUri();
-
-            if (in_array($linkUrl, self::$crawledUrls, true) === true) {
-                continue;
-            }
-
-            $this->crawl($linkUrl, $link);
-        }
-
-        $this->em->flush();
-    }
-
-    protected function updateLinkWithResponse(Link $link, ResponseInterface $response): void
-    {
-        $statusCode = $this->resolveStatusCode($response);
-
-        $this->registry->get($link)->apply(
-            $link,
-            $this->resolveTransitionByResponse($statusCode)
-        );
-    }
-
-
-    protected function resolveStatusCode(ResponseInterface $response): ?int
-    {
-        try {
-            $statusCode = $response->getStatusCode();
-        } catch (Throwable $e) {
-            $statusCode = null;
-        }
-
-        return $statusCode;
-    }
-
-    /**
-     * @param int|null $statusCode
-     * @return string
-     */
-    private function resolveTransitionByResponse(?int $statusCode): string
-    {
-        if ($statusCode === null) {
-            return Link::TRANSITION_FAILING;
-        }
-
-        if ($statusCode >= 500) {
-            return Link::TRANSITION_DYING;
-        }
-
-        if ($statusCode >= 400) {
-            return Link::TRANSITION_FAILING;
-        }
-
-        if ($statusCode >= 300) {
-            return Link::TRANSITION_REDIRECTING;
-        }
-
-        if ($statusCode >= 200) {
-            return Link::TRANSITION_SUCCESS;
-        }
-
-        return Link::TRANSITION_FAILING;
-    }
-
-    protected function resolveContent(ResponseInterface $response): ?string
-    {
-        try {
-            $content = $response->getContent();
-        } catch (Throwable $e) {
-            $content = null;
-        }
-
-        return $content;
-    }
-
-    protected function fetchOrCreateLink(string $url, ?Link $parentLink): Link
-    {
-        $existingLink = $this->em->getRepository(Link::class)->findOneBy(['link' => $url]);
-
-        if ($existingLink !== null) {
-            return $existingLink;
-        }
-
-        return $this->createLink($url, $parentLink);
-    }
-
-    protected function createLink(string $url, ?Link $parentLink): Link
-    {
-        $newLink = new Link();
-        $newLink->setLink($url);
-
-        if ($parentLink !== null) {
-            $newLink->addParentLink($parentLink);
-        }
-
-        return $newLink;
+        $this->responseParser->parseResponse($link, $response);
     }
 }
